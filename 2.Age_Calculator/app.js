f320dc9512726b23a7ad8f5f0fe479e93fc4b808
mack <mackparker001@gmail.com>
2021-05-12 18:23:57 +0530

    age Calculator

---
+++ 2.Age_Calculator/app.js
@@ -0,0 +1,29 @@
+function age() {
+  var inputAge = document.getElementById('date_birth').value
+
+  var ud = Date.parse(inputAge)
+  var nd = Date.now()
+
+  var milliSec = nd - ud
+
+  var min = 1000 * 60
+  var hrs = min * 60
+  var day = hrs * 24
+  var year = day * 365
+
+  var age = Math.round(milliSec / year)
+  var month = age * 12
+  var days = age * 365
+  var hour = Math.round(milliSec / hrs)
+  var sec = Math.round(milliSec / 1000)
+
+  document.getElementById('age').innerText = age
+  document.getElementById('month').innerText = month
+  document.getElementById('days').innerText = days
+  document.getElementById('hour').innerText = hour
+  document.getElementById('sec').innerText = sec
+
+  document.getElementById(
+    'result'
+  ).innerText = `${yy}, ${month}, ${days},${hour}, ${sec}`
+}
--- learn/try.js
+++
@@ -1,9 +0,0 @@
-var realanme = '2'
-var nam2 = 2
-var result = ''
-
-if (realanme === nam2) {
-  console.log('ripon')
-} else {
-  console.log('hey fuck boy')
-}
